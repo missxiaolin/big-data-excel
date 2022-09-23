f4af7f3211ae94a737d46e0c94fb05ef022c2230
lingongbing <462441355@qq.com>
2022-09-23 14:17:00 +0800

    webpack 配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,32 @@
-const { defineConfig } = require('@vue/cli-service')
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const { defineConfig } = require('@vue/cli-service');
+
+// function addStyleResource(rule) {
+//   rule
+//     .use('style-resource')
+//     .loader('style-resources-loader')
+//     .options({
+//       patterns: [path.resolve(__dirname, './src/styles/variables/*.less'), path.resolve(__dirname, './src/styles/mixins/*.less')],
+//     });
+// }
+
+
 module.exports = defineConfig({
   lintOnSave:false,
-  transpileDependencies: true
+  transpileDependencies: true,
+  productionSourceMap: false, // 去除生产环境的 productionSourceMap
+  publicPath: './',
+  chainWebpack: (config) => {
+    // 修改项目名称
+    config.plugin('html').tap((args) => {
+      args[0].title = 'xlsx-study'; // 项目名称
+      return args;
+    });
+    // 配置别名
+    config.resolve.alias.set('@', path.resolve('src'));
+    // 自动引入全局样式
+    // const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    // types.forEach((type) => addStyleResource(config.module.rule('less').oneOf(type)));
+  },
 })
