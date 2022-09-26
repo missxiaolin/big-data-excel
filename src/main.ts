3649b4cf63ad50ef627c13e0339bacbd71b05e41
lingongbing <462441355@qq.com>
2022-09-26 10:17:50 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import VueCompositionAPI from '@vue/composition-api';
 import { createPinia, PiniaVuePlugin } from 'pinia';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
@@ -12,11 +11,9 @@ import store from './store'
 Vue.use(ElementUI);
 Vue.prototype.$video = videojs;
 Vue.config.productionTip = false;
-Vue.use(VueCompositionAPI);
 
 Vue.use(PiniaVuePlugin);
 const pinia = createPinia();
-
 
 Vue.config.productionTip = false
 
